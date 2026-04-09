9124beb52537096dc9cae18ff9cdb9adc53769e9
Josh Holtz <me@joshholtz.com>
2026-04-09 16:22:52 -0500

    fix: don't auto-focus input when selecting a bee

--- web/src/components/Briefing.tsx
+++ web/src/components/Briefing.tsx
@@ -285,7 +285,6 @@ export default function Briefing({
                     setTargetWorkspace(entry.workspace);
                     setTargetBee(entry.bee);
                     setHiveOpen(false);
-                    inputRef.current?.focus();
                   }}
                 >
                   <span style={{
