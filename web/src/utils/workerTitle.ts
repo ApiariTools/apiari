33d958edc4b65b4fbd98923e4d7b3893eeba00bb
Josh Holtz <me@joshholtz.com>
2026-05-08 13:29:37 -0500

    test: address PR review feedback

--- web/src/utils/workerTitle.ts
+++ web/src/utils/workerTitle.ts
@@ -1,5 +1,5 @@
-import type { WorkerV2 } from "../types";
+import type { WorkerV2 } from '../types'
 
-export function getWorkerTitle(worker: Pick<WorkerV2, "display_title" | "goal" | "branch" | "id">): string {
-  return worker.display_title ?? worker.goal ?? worker.branch ?? worker.id;
+export function getWorkerTitle(worker: Pick<WorkerV2, 'display_title' | 'goal' | 'branch' | 'id'>): string {
+  return worker.display_title ?? worker.goal ?? worker.branch ?? worker.id
 }
